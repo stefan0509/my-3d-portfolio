4a4c6e7c520a7ca2fa8815e080da9fcb2f88f5da
stefan0509 <>
2024-07-12 10:21:58 -0400

    New footer change

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -25,6 +25,7 @@ const RecentProjects = () => {
               <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
                 <div
                   className="relative w-full h-full overflow-hidden lg:rounded-3xl"
+                  
                   style={{ backgroundColor: "#13162D" }}
                 >
                   <img src="/bg.png" alt="bgimg" />
