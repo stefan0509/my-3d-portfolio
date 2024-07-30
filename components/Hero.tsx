dd6280845a08ea8a57677aa276dfd4808839d666
stefan <stefanupwork49@gmail.com>
2024-07-30 12:53:43 +0300

    Update country

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -57,7 +57,7 @@ const Hero = () => {
           />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi! I&apos;m Stefan Varava, a Software Developer based in Canada.
+            Hi! I&apos;m Stefan Varava, a Software Developer based in Romania.
           </p>
 
           <a href="#about">
